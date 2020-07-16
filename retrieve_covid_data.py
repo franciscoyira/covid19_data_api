5f99e6a672ae0ff4730eb5db7f822e9bf510c80b
Francisco Yira <francisco.yira@outlook.com>
2020-07-16 19:23:58 -0400

    PoC updating data of one country (Chile)

--- retrieve_covid_data.py
+++ retrieve_covid_data.py
@@ -58,6 +58,75 @@ yesterday_str = yesterday.strftime("%Y-%m-%d") + 'T00:00:00Z'
 
 # Create URL 
 if last_updates[py_id] is None:
-    request_url = base_url + slugs[194] + '/status/confirmed?from=' + '2020-03-01T00:00:00Z' + '&to=' + yesterday_str
+    request_url_confirmed = base_url + slugs[py_id] + '/status/confirmed?from=' + '2020-03-01T00:00:00Z' + '&to=' + yesterday_str
 else:
-    request_url = base_url + slugs[194] + '/status/confirmed?from=' + last_updates[py_id] + 'T00:00:00Z' + '&to=' + yesterday_str
+    request_url_confirmed = base_url + slugs[py_id] + '/status/confirmed?from=' + last_updates[py_id] + 'T00:00:00Z' + '&to=' + yesterday_str
+
+# Create URL variations for recovered and deaths
+request_url_deaths = request_url_confirmed.replace('confirmed', 'deaths')
+request_url_recovered = request_url_confirmed.replace('confirmed', 'recovered')
+
+# Retrieve data using the request URLs
+# Confirmed
+conn_confirmed = urlopen(request_url_confirmed, context=ctx)
+data_confirmed = conn_confirmed.read().decode()
+js_confirmed = json.loads(data_confirmed)
+
+# Process data and save it in lists
+dates = list()
+confirmed = list()
+
+for element in js_confirmed:
+    dates.append(element['Date'][:10])
+    confirmed.append(int(element['Cases']))
+
+# Deaths
+conn_deaths = urlopen(request_url_deaths, context=ctx)
+data_deaths = conn_deaths.read().decode()
+js_deaths = json.loads(data_deaths)
+
+deaths = list()
+
+for element in js_deaths:
+    deaths.append(int(element['Cases']))
+
+# Recovered
+conn_recovered = urlopen(request_url_recovered, context=ctx)
+data_recovered = conn_recovered.read().decode()
+js_recovered = json.loads(data_recovered)
+
+recovered = list()
+
+for element in js_recovered:
+    recovered.append(int(element['Cases']))
+
+# Create tuples to insert in DB
+#  (country_id, datetime, confirmed, recovered, deaths)
+
+to_insert = list()
+for i in range(len(dates)):
+    t = (py_id+1, dates[i], confirmed[i], recovered[i], deaths[i])
+    to_insert.append(t)
+
+# Query to insert values
+insert_query = '''
+INSERT INTO Cases
+(country_id, datetime, confirmed, recovered, deaths)
+VALUES (?, ?, ?, ?, ?)'''
+
+cur.executemany(insert_query, to_insert)
+
+# Update 'last_updated' field in Countries table
+# Get today's date
+today_str = datetime.today().strftime("%Y-%m-%d") + 'T00:00:00Z'
+country_id = py_id + 1
+
+cur.execute(
+    '''UPDATE Countries
+    SET last_updated = ?
+    WHERE id = ?''',
+    (today_str, country_id))
+
+
+conn.commit()
+cur.close()
